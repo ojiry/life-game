3830269cf4e2090b16982354ee3d2dd96e1b6b21
Ryoji Yoshioka <yoshioka@decult.org>
2011-12-08 13:50:40 +0900

    displayメソッドを作成

--- life_game.rb
+++ life_game.rb
@@ -5,4 +5,16 @@ class Grid
     @width = width
     @map = map
   end
+
+  def display
+    @height.times do |i|
+      @width.times do |j|
+        print @map.include?([i, j]) ? "*" : "-"
+      end
+      print "\n"
+    end
+  end
 end
+
+grid = Grid.new(3, 3, [[0, 0], [0, 1], [1, 0]])
+grid.display
